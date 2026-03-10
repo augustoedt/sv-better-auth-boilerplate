ee00b64824b7794d899831b9532f2d63f2bf92ea
Augusto Eduardo Torres <augustoedt@gmail.com>
2026-03-10 18:36:23 -0300

    refactor: Modularize Drizzle schemas by separating authentication tables into `auth.schema.ts` and updating dependencies.

---
+++ src/lib/server/db/app.schema.ts
@@ -0,0 +1,7 @@
+// import { pgTable, text, boolean } from "drizzle-orm/pg-core";
+
+// export const todo = pgTable("todo", {
+//   id: text("id").primaryKey(),
+//   description: text("description").notNull(),
+//   completed: boolean("completed").default(false).notNull(),
+// });
